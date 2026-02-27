e49ae13331de6cac443f85f2851e4d66131a12cf
sohamkarkhanis <sohmkarkhanis@gmail.com>
2026-02-26 22:50:55 -0800

    dashboard done

--- scripts/dataset_filter.py
+++ scripts/dataset_filter.py
@@ -4,7 +4,7 @@ import pandas as pd
 def process_reviews(input_meta_filename = False, metadata_fields_to_keep = ['parent_asin','main_category',"title", 'store',"average_rating", "rating_number"],*,  input_review_filename, save_meta = True,
                     review_fields_to_keep):
     '''Reads provided review and metadata json files and returns a filtered csv based on fields that have been selected
-    
+
     Inputs:
     input_review_filename: The review filename + path
     input_meta_filename: The meta filename + path
@@ -18,15 +18,15 @@ def process_reviews(input_meta_filename = False, metadata_fields_to_keep = ['par
     rating	         |float	|Rating of the product (from 1.0 to 5.0).
     title	         |str	|Title of the user review.
     text	         |str	|Text body of the user review.
-    images	         |int	|Number of images that are attached to the review 
-    parent_asin	     |str	|Parent ID of the product. 
+    images	         |int	|Number of images that are attached to the review
+    parent_asin	     |str	|Parent ID of the product.
     user_id	         |str	|ID of the reviewer
     timestamp	     |int	|Tuple in the form of (MM/DD/YYYY)]
     verified_purchase|bool	|User purchase verification
     helpful_vote	 |int	|Helpful votes of the review
 
     '''
-    
+
     df_reviews = pd.read_json(input_review_filename.replace('\\', '/'),lines=True)
 
     if 'images' in review_fields_to_keep:
@@ -34,11 +34,11 @@ def process_reviews(input_meta_filename = False, metadata_fields_to_keep = ['par
 
     if 'timestamp' in review_fields_to_keep:
         df_reviews['timestamp'] = df_reviews['timestamp'].apply(lambda x: (x.month, x.day, x.year))  #Save timestamp data in terms of month, day, year
-    
+
     if 'text' in review_fields_to_keep:
         df_reviews['text_length'] = df_reviews["text"].str.split().str.len()  #Get text length
         review_fields_to_keep.append('text_length')
-    
+
     if input_meta_filename and save_meta:
         review_fields_to_keep.append('parent_asin')
 
@@ -61,14 +61,14 @@ def process_reviews(input_meta_filename = False, metadata_fields_to_keep = ['par
 
 
 
-### EXAMPLE 
-### NEED THESE FILES TO RUN
-input_file_review = "All_Beauty.jsonl/All_Beauty.jsonl"
-input_file_meta = "meta_All_Beauty.jsonl/meta_All_Beauty.jsonl"
-###
+# ### EXAMPLE
+# ### NEED THESE FILES TO RUN
+# input_file_review = "All_Beauty.jsonl/All_Beauty.jsonl"
+# input_file_meta = "meta_All_Beauty.jsonl/meta_All_Beauty.jsonl"
+# ###
 
-print(process_reviews(
-    input_review_filename = input_file_review,
-    input_meta_filename= input_file_meta,                      
-    review_fields_to_keep= ['helpful_vote','timestamp','rating','images','text'] 
-).head(6))
+# print(process_reviews(
+#     input_review_filename = input_file_review,
+#     input_meta_filename= input_file_meta,
+#     review_fields_to_keep= ['helpful_vote','timestamp','rating','images','text']
+# ).head(6))
