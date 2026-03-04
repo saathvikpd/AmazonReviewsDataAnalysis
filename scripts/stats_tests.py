85a6c426877690d81e1dbf1dbdbd51fa0bebbafa
sohamkarkhanis <sohmkarkhanis@gmail.com>
2026-03-04 13:59:17 -0800

    updated paths

--- scripts/stats_tests.py
+++ scripts/stats_tests.py
@@ -5,18 +5,18 @@ import statsmodels.api as sm
 from statsmodels.tsa.stattools import grangercausalitytests
 
 def compute_corr(time, num_reviews, helpful_votes, max_lag = 50, category = "All_Beauty"):
-    
+
     df = pd.DataFrame({
         "reviews": num_reviews,
         "helpful": helpful_votes
     })
-    
+
     results = {}
-    
+
     for lag in range(0, max_lag):
         df[f"helpful_lag{lag}"] = df["helpful"].shift(lag)
         results[-lag] = df["reviews"].corr(df[f"helpful_lag{lag}"])
-    
+
     corr_data = results.items()
     corr_data = sorted(results.items(), key = lambda x: x[0])
 
@@ -25,7 +25,8 @@ def compute_corr(time, num_reviews, helpful_votes, max_lag = 50, category = "All
     plt.xlabel("Time Lag")
     plt.ylabel("Correlation")
     plt.title(f"Corr b/w num reviews & lagged helpful votes, Category: '{category}")
-    plt.savefig(f"plots/corr_plot_{category}.png")
+    plt.savefig(os.path.join(PLOTS_PATH, f"plots/corr_plot_{category}.png"), dpi=600)
+    #plt.savefig(f"plots/corr_plot_{category}.png")
 
     peak_lag = -max(corr_data, key = lambda x: x[1])[0]
 
@@ -41,9 +42,9 @@ def compute_OLS(time, num_reviews, helpful_votes, lag = 1):
 
     df[f"reviews_lag{lag}"] = df["reviews"].shift(lag)
     df[f"helpful_lag{lag}"] = df["helpful"].shift(lag)
-    
+
     df = df.dropna()
-        
+
     X = df[[f"reviews_lag{lag}", f"helpful_lag{lag}"]]
     X = sm.add_constant(X)
 
@@ -58,9 +59,8 @@ def compute_granger(time, num_reviews, helpful_votes, num_lag = 1, category = "A
         "helpful": helpful_votes
     })
 
-
     data = df[["reviews", "helpful"]]
-    
+
     results = grangercausalitytests(data, maxlag = num_lag)
 
     p_values = []
@@ -77,7 +77,8 @@ def compute_granger(time, num_reviews, helpful_votes, num_lag = 1, category = "A
     plt.xlabel("Lag")
     plt.ylabel("Granger causality p-val")
     plt.title(f"Granger causality p-vals vs lag, Category: '{category}")
-    plt.savefig(f"plots/granger_{category}.png")
+    plt.savefig(os.path.join(PLOTS_PATH, f"plots/granger_{category}.png"), dpi=600)
+    # plt.savefig(f"plots/granger_{category}.png")
 
     return results
 
@@ -99,4 +100,3 @@ def run_tests(time, num_reviews, helpful_votes, category = "All_Beauty"):
     print("Running Granger Causality test...")
 
     test_results = compute_granger(time, num_reviews, helpful_votes, num_lag = peak_lag, category = category)
-    
